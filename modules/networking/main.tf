6ad677ae5047fe3b00eceeac98a158e2f870f273
AchourOussama <oachour08@gmail.com>
2024-12-10 13:37:04 +0100

    Clean: terraform config ; Test: Jenkinsfile

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -28,32 +28,3 @@ resource "azurerm_subnet" "subnets" {
   }
 }
 
-# # Subnet for the Frontend
-# resource "azurerm_subnet" "frontend_subnet" {
-#   name                 = var.frontend_subnet_name
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefixes     = var.frontend_subnet_address_space
-#   delegation {
-#     name = "delegation"
-#     service_delegation {
-#       name = "Microsoft.Web/serverFarms"
-#     }
-#   } 
-# }
-
-# # Subnet for the Backend
-# resource "azurerm_subnet" "backend_subnet" {
-#   name                 = var.backend_subnet_name
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefixes     = var.backend_subnet_address_space
-#   private_endpoint_network_policies = "Disabled"
-# }
-
-# # Virtual Network Integration for the frontend
-# resource "azurerm_app_service_virtual_network_swift_connection" "vnetintegrationconnection" {
-#   app_service_id  = azurerm_linux_web_app.frontend_web_app.id
-#   subnet_id       = azurerm_subnet.frontend_subnet.id
-# }
-

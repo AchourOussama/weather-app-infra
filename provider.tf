ec29dd349ad0bf5756307df7c0f83cd2a19a2ce1
AchourOussama <oachour08@gmail.com>
2024-12-10 14:52:20 +0100

    Update: Jenkinsfile

--- provider.tf
+++ provider.tf
@@ -8,5 +8,9 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
+  # subscription_id = var.subscription_id
+  client_id       = var.AZURE_CLIENT_ID
+  client_secret   = var.AZURE_CLIENT_SECRET
+  tenant_id       = var.AZURE_TENANT_ID
+  subscription_id = var.AZURE_SUBSCRIPTION_ID
 }
--- variables.tf
+++ variables.tf
@@ -3,8 +3,23 @@ variable "location" {
     description = "Region where to deploy resources"
   
 }
-variable "subscription_id" {
+variable "AZURE_SUBSCRIPTION_ID" {
     type = string
     sensitive = true
     description = "Subscription id of the user"
+}
+variable "AZURE_TENANT_ID" {
+    type = string
+    sensitive = true
+    description = "Tenant id of the user"
+}
+variable "AZURE_CLIENT_SECRET" {
+    type = string
+    sensitive = true
+    description = "Password of the service principal"
+}
+variable "AZURE_CLIENT_ID" {
+    type = string
+    sensitive = true
+    description = "Service principal id "
 }

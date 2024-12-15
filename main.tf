627c4c2094c68c7db90f98909882a3b5c19c50a6
AchourOussama <oachour08@gmail.com>
2024-12-15 21:11:06 +0100

    Fix: networking module variables issue

--- main.tf
+++ main.tf
@@ -4,25 +4,25 @@ module "resource_group" {
   location = var.location
 }
 
-module "virtual_network" {
-  source              = "./modules/networking"
-  name                = "weather-app-net"
-  resource_group_name = module.resource_group.name
-  location            = module.resource_group.location
-  address_space       = ["10.0.0.0/16"]
-  subnets = {
-    "frontend-subnet" = {
-      address_prefix        = "10.0.1.0/24"
-      delegation_name       = "delegation"
-      delegation_service_name = "Microsoft.Web/serverFarms"
-    },
-    "backend-subnet" = {
-      address_prefix        = "10.0.2.0/24"
-      delegation_name       = "delegation"
-      delegation_service_name = "Microsoft.Web/serverFarms"
-    }
-  }
-}
+# module "virtual_network" {
+#   source              = "./modules/networking"
+#   name                = "weather-app-net"
+#   resource_group_name = module.resource_group.name
+#   location            = module.resource_group.location
+#   address_space       = ["10.0.0.0/16"]
+#   subnets = {
+#     "frontend-subnet" = {
+#       address_prefix        = "10.0.1.0/24"
+#       delegation_name       = "delegation"
+#       delegation_service_name = "Microsoft.Web/serverFarms"
+#     },
+#     "backend-subnet" = {
+#       address_prefix        = "10.0.2.0/24"
+#       delegation_name       = "delegation"
+#       delegation_service_name = "Microsoft.Web/serverFarms"
+#     }
+#   }
+# }
 
 module "app_services" {
   source               = "./modules/app-services"
--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,8 +1,8 @@
 # Virtual Network 
 resource "azurerm_virtual_network" "vnet" {
   name                = var.name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
   address_space       = var.address_space
 }
 
